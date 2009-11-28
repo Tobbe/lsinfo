978e98c7ff9bea11babfd827327525bb6a7d3375
Tobbe Lundberg <tobbesweden@gmail.com>
2009-11-28 12:30:30 -0500

    Made PDO throw an exception on errors (for example malformed SQL)

--- lib/applicationfactory.php
+++ lib/applicationfactory.php
@@ -7,9 +7,10 @@ class ApplicationFactory {
 	public $pdo_dsn;
 	public $pdo_username;
 	public $pdo_password;
+	public $pdo_attributes;
 
 	function new_PDO($c) {
-		return new PDO($this->pdo_dsn, $this->pdo_username, $this->pdo_password);
+		return new PDO($this->pdo_dsn, $this->pdo_username, $this->pdo_password, $this->pdo_attributes);
 	}
 
 	function new_k_TemplateFactory($c) {
